5b8acbed307974142c669b75919ab6482c790abd
Milton Wong <amiltonwong@gmail.com>
2017-04-05 13:41:42 +0800

    commit

---
+++ pipeline_hog_det.py
@@ -0,0 +1,40 @@
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+# for scikit-learn >= 0.18 use:
+from sklearn.model_selection import train_test_split
+# from sklearn.cross_validation import train_test_split
+from scipy.ndimage.measurements import label
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+import numpy as np
+import pickle
+import cv2
+import glob
+import time
+
+#Load Training Data
+car_images = glob.glob('training_data/vehicles/**/*.png')
+noncar_images = glob.glob('training_data/non-vehicles/**/*.png')
+
+# HoG feature extraction function, reference from course material
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
+                        vis=False, feature_vec=True):
+    # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, 
+                                  pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), 
+                                  transform_sqrt=False, 
+                                  visualise=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:      
+        features = hog(img, orientations=orient, 
+                       pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), 
+                       transform_sqrt=False, 
+                       visualise=vis, feature_vector=feature_vec)
+        return features
